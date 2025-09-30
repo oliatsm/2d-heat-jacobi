88f4773a55d9b29e90127e15a91733c61b5142ba
oliatsm <olia.tsm@gmail.com>
2025-09-30 13:03:03 +0300

    reduction: remove modulo

--- cuda/laplace2d.cu
+++ cuda/laplace2d.cu
@@ -1,3 +1,29 @@
+/* Copyright (c) 2012, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of NVIDIA CORPORATION nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include <stdio.h>
 #include <math.h>
@@ -122,17 +148,19 @@ void max_reduce(double *A, double *Anew, int m, int n, double *d_max){
     int tid = threadIdx.x;
     int i = blockIdx.x * blockDim.x + threadIdx.x;
 
-    double diff = 0.0;
-    if (i < n*m) diff = fabs(A[i] - Anew[i]);
-    sdata[tid] = diff;
+    sdata[tid] = (i < m*n) ? fabs(A[i] - Anew[i]) : 0;
 
     __syncthreads();
 
     // Calculate max for each block
+    //  step s doesn't exceed blockDim, 
+    //  so index+s is inside the block
     for(int s = 1; s < blockDim.x; s *= 2){
-        if (tid % (2 * s) == 0) {
-            if(sdata[tid] < sdata[tid + s])
-                sdata[tid] = sdata[tid + s];   
+        int index = 2 * s * tid;
+
+        if (index < blockDim.x) {
+            if(sdata[index] < sdata[index + s])
+            sdata[index] = sdata[index + s];
         }
         __syncthreads();
     }
